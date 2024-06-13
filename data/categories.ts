f6922c9cadddf0d431a1f4a26829d91ec3d71613
Rizky Aditya Syahputra <93401116+Ras223@users.noreply.github.com>
2024-06-13 19:09:35 +0700

    first commit

---
+++ data/categories.ts
@@ -0,0 +1,24 @@
+const destinationCategories = [
+  {
+    title: "All",
+    iconName: "hiking",
+  },
+  {
+    title: "Coffe",
+    iconName: "beach",
+  },
+  {
+    title: "Non-Coffe",
+    iconName: "terrain",
+  },
+  {
+    title: "Teh",
+    iconName: "city",
+  },
+  {
+    title: "Makanan",
+    iconName: "tree",
+  },
+];
+
+export default destinationCategories;
